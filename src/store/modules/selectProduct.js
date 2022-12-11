229eea164eb58d52e60d27afd548c690a4b6aedc
shifat514 <shifat514@gmail.com>
2022-12-11 20:18:30 +0600

    add  quantity section

--- src/store/modules/selectProduct.js
+++ src/store/modules/selectProduct.js
@@ -6,19 +6,26 @@ const state = {
 const getters = {
     getSelectList: state => state.selectList,
     selectListLength: state => state.selectList.length,
-    
+
 };
 
 const mutations = {
-    addMenu(state,payload)
-    {
+    addMenu(state, payload) {
         state.selectList.push(payload);
+    },
+    removeFromMenu(state,payload) {
+        state.selectList.splice(payload,1);
     }
+
 };
 
 const actions = {
     selectMenu(context, payload) {
         context.commit("addMenu", payload);
+    },
+
+    removeProduct(context,payload) {
+        context.commit("removeFromMenu",payload);
     }
 };
 
